5614eb5cb761315caafec66740b3d5c0ae35b515
Bryan Wehner <bwehner@ccs.neu.edu>
2019-02-01 00:44:50 -0500

    Added service category service

---
+++ src/main/java/com/example/cs4500_sp19_the_business_side/services/ServiceCateoryService.java
@@ -0,0 +1,7 @@
+package com.example.cs4500_sp19_the_business_side.services;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceCateoryService {
+}
