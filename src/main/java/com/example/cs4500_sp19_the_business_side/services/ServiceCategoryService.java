c8e20c1940318f01f059510c39563954691d418f
Bryan Wehner <bwehner@ccs.neu.edu>
2019-02-01 00:46:22 -0500

    Added add all endpoint

--- src/main/java/com/example/cs4500_sp19_the_business_side/services/ServiceCategoryService.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/services/ServiceCategoryService.java
@@ -1,7 +1,20 @@
 package com.example.cs4500_sp19_the_business_side.services;
 
+import com.example.cs4500_sp19_the_business_side.models.ServiceCategory;
+import com.example.cs4500_sp19_the_business_side.repositories.ServiceCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ServiceCategoryService {
+    @Autowired
+    ServiceCategoryRepository serviceRepository;
+    @GetMapping("/api/categories")
+    public List<ServiceCategory> findAllServiceCategories() {
+        return serviceRepository.findAllServiceCategories();
+    }
+
 }
