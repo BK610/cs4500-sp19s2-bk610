a57db70a31e625be1621bdfe475180f7ca67a7c8
Bailey Kane <bailey.orion.kane@gmail.com>
2019-02-01 19:31:26 -0500

    Basic class setup

---
+++ src/main/java/com/example/cs4500_sp19_the_business_side/services/ServiceService.java
@@ -0,0 +1,12 @@
+package com.example.cs4500_sp19_the_business_side.services;
+
+import com.example.cs4500_sp19_the_business_side.models.Service;
+import com.example.cs4500_sp19_the_business_side.repositories.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class ServiceService {
+    
+}
