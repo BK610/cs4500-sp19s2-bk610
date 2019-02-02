a58a3835fae08c63185eb1b7e3b7a52c6d3a653b
Bailey Kane <bailey.orion.kane@gmail.com>
2019-02-01 19:31:58 -0500

    Created CRUD service functions

--- src/main/java/com/example/cs4500_sp19_the_business_side/services/ServiceService.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/services/ServiceService.java
@@ -8,5 +8,32 @@ import java.util.List;
 
 @RestController
 public class ServiceService {
-    
+    @Autowired
+    ServiceRepository serviceRepository;
+    @GetMapping("/api/services")
+    public List<Service> findAllService() {
+        return serviceRepository.findAllServices();
+    }
+    @GetMapping("/api/services/{serviceId}")
+    public Service findServiceById(
+            @PathVariable("serviceId") Integer id) {
+        return serviceRepository.findServiceById(id);
+    }
+    @PostMapping("/api/services")
+    public Service createService(@RequestBody Service service) {
+        return serviceRepository.save(service);
+    }
+    @PutMapping("/api/services/{serviceId}")
+    public Service updateService(
+            @PathVariable("serviceId") Integer id,
+            @RequestBody Service serviceUpdates) {
+        Service service = serviceRepository.findServiceById(id);
+        service.setServiceName(serviceUpdates.getServiceName());
+        return serviceRepository.save(service);
+    }
+    @DeleteMapping("/api/services/{serviceId}")
+    public void deleteService(
+            @PathVariable("serviceId") Integer id) {
+        serviceRepository.deleteById(id);
+    }
 }
