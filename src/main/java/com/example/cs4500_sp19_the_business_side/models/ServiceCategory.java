dc33f0d985f345f76dbfef99052fc8bb3feaae3f
Nick Bond <bond.n@husky.neu.edu>
2019-02-05 21:18:18 -0500

    updates ServiceCategory data model

--- src/main/java/com/example/cs4500_sp19_the_business_side/models/ServiceCategory.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/models/ServiceCategory.java
@@ -1,10 +1,17 @@
 package com.example.cs4500_sp19_the_business_side.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="service_categories")
@@ -12,17 +19,30 @@ public class ServiceCategory {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	private String serviceCategoryName;
+	private String title;
+	@ManyToMany
+	@JsonIgnore
+	@JoinTable(
+			name="CATEGORIES_SERVICES",
+			joinColumns=@JoinColumn(name="CATEGORY_ID", referencedColumnName="ID"),
+			inverseJoinColumns=@JoinColumn(name="SERVICE_ID", referencedColumnName="ID"))
+	private List<Service> services;
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getServiceCategoryName() {
-		return serviceCategoryName;
+	public String getTitle() {
+		return title;
 	}
-	public void setServiceCategoryName(String serviceCategoryName) {
-		this.serviceCategoryName = serviceCategoryName;
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public List<Service> getServices() {
+		return services;
+	}
+	public void setServices(List<Service> services) {
+		this.services = services;
 	}
 }
