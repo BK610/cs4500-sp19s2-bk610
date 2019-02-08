ab474550d221141eb51023f8ea3a0cbedbe24be7
Catherine McLean <cmclean@ccs.neu.edu>
2019-02-07 21:53:34 -0500

    added a new class for the service specific question service

---
+++ src/main/java/com/example/cs4500_sp19_the_business_side/services/ServiceSpecificQuestionService.java
@@ -0,0 +1,20 @@
+package com.example.cs4500_sp19_the_business_side.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cs4500_sp19_the_business_side.models.Service;
+import com.example.cs4500_sp19_the_business_side.repositories.ServiceRepository;
+
+@RestController
+public class ServiceSpecificQuestionService {
+
+}
