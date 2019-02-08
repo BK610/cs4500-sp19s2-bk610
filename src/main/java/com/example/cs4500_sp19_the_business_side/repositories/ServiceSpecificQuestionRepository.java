f2e94a3bc6a286c1bbda37ae43cfb2f49041a810
Catherine McLean <cmclean@ccs.neu.edu>
2019-02-07 22:11:04 -0500

    fixed a naming issue in the question repository

--- src/main/java/com/example/cs4500_sp19_the_business_side/repositories/ServiceSpecificQuestionRepository.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/repositories/ServiceSpecificQuestionRepository.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.example.cs4500_sp19_the_business_side.models.Service;
+import com.example.cs4500_sp19_the_business_side.models.ServiceSpecificQuestion;
 
 public interface ServiceSpecificQuestionRepository {
 	@Query(value="SELECT serviceSpecificQuestion FROM ServiceSpecificQuestion serviceSpecificQuestion")
-	public List<Service> findAllServiceSpecificQuestions();
+	public List<ServiceSpecificQuestion> findAllServiceSpecificQuestions();
 	@Query(value="SELECT serviceSpecificQuestion FROM ServiceSpecificQuestion serviceSpecificQuestion WHERE serviceSpecificQuestion.id=:id")
-	public Service findServiceSpecificQuestionById(@Param("id") Integer id);
+	public ServiceSpecificQuestion findServiceSpecificQuestionById(@Param("id") Integer id);
 }
