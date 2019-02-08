97a94b99585705809cf1009cef6a6ecee1f57604
Catherine McLean <cmclean@ccs.neu.edu>
2019-02-07 22:14:52 -0500

    extend the repository by the CRUD repo

--- src/main/java/com/example/cs4500_sp19_the_business_side/repositories/ServiceSpecificQuestionRepository.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/repositories/ServiceSpecificQuestionRepository.java
@@ -3,11 +3,13 @@ package com.example.cs4500_sp19_the_business_side.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import com.example.cs4500_sp19_the_business_side.models.Service;
 import com.example.cs4500_sp19_the_business_side.models.ServiceSpecificQuestion;
 
-public interface ServiceSpecificQuestionRepository {
+public interface ServiceSpecificQuestionRepository extends CrudRepository<ServiceSpecificQuestion, Integer> {
 	@Query(value="SELECT serviceSpecificQuestion FROM ServiceSpecificQuestion serviceSpecificQuestion")
 	public List<ServiceSpecificQuestion> findAllServiceSpecificQuestions();
 	@Query(value="SELECT serviceSpecificQuestion FROM ServiceSpecificQuestion serviceSpecificQuestion WHERE serviceSpecificQuestion.id=:id")
