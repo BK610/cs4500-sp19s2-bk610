ca07b906ac3958eaa9219df1601db31d456e67dc
Catherine McLean <cmclean@ccs.neu.edu>
2019-02-07 21:50:42 -0500

    made the service specific question repository code

--- src/main/java/com/example/cs4500_sp19_the_business_side/repositories/ServiceSpecificQuestionRepository.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/repositories/ServiceSpecificQuestionRepository.java
@@ -1,5 +1,15 @@
 package com.example.cs4500_sp19_the_business_side.repositories;
 
-public class ServiceSpecificQuestionRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.cs4500_sp19_the_business_side.models.Service;
+
+public interface ServiceSpecificQuestionRepository {
+	@Query(value="SELECT serviceSpecificQuestion FROM ServiceSpecificQuestion serviceSpecificQuestion")
+	public List<Service> findAllServiceSpecificQuestions();
+	@Query(value="SELECT serviceSpecificQuestion FROM ServiceSpecificQuestion serviceSpecificQuestion WHERE serviceSpecificQuestion.id=:id")
+	public Service findServiceSpecificQuestionById(@Param("id") Integer id);
 }
