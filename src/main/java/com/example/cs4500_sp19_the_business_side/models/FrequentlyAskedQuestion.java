d04bf3790f69236b726f4569c04e7ac939e94137
Nikhil <nikhilphatak1@gmail.com>
2019-02-08 16:01:22 -0500

    FAQ Questions code completed (uncommented).

--- src/main/java/com/example/cs4500_sp19_the_business_side/models/FrequentlyAskedQuestion.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/models/FrequentlyAskedQuestion.java
@@ -17,15 +17,14 @@ public class FrequentlyAskedQuestion {
     private Integer id;
     private String title;
     private String question;
-    // TODO: Uncomment this when FAQ answers are implemented
-//    @OneToMany(mappedBy="frequentlyAskedQuestion")
-//    private List<FrequentlyAskedAnswer> answers;
-//    public List<FrequentlyAskedAnswer> getAnswers() {
-//        return answers;
-//    }
-//    public void setAnswers(List<FrequentlyAskedAnswer> answers) {
-//        this.answers = answers;
-//    }
+    @OneToMany(mappedBy="frequentlyAskedQuestion")
+    private List<FrequentlyAskedAnswer> answers;
+    public List<FrequentlyAskedAnswer> getAnswers() {
+        return answers;
+    }
+    public void setAnswers(List<FrequentlyAskedAnswer> answers) {
+        this.answers = answers;
+    }
     public Integer getId() {
         return id;
     }
