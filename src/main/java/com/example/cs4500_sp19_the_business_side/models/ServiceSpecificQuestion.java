282c82d8a8a1b5021873843b8851d71dd6d4f2fd
Bryan Wehner <bwehner@ccs.neu.edu>
2019-02-07 20:35:52 -0500

    Added service specific question

--- src/main/java/com/example/cs4500_sp19_the_business_side/models/ServiceSpecificQuestion.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/models/ServiceSpecificQuestion.java
@@ -1,4 +1,66 @@
 package com.example.cs4500_sp19_the_business_side.models;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="service_specific_questions")
 public class ServiceSpecificQuestion {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne //TODO bwehner correct?
+    private ServiceCategory serviceCategory;
+//    @OneToMany TODO uncomment once service specific answers is implemented
+//    private List<ServiceSpecificAnswer> serviceSpecificAnswers;
+    @Enumerated(EnumType.STRING)
+    private QuestionType questionType;
+    private String defaultValue;
+    private String possibleChoices;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ServiceCategory getServiceCategory() {
+        return serviceCategory;
+    }
+
+    public void setServiceCategory() {
+        this.serviceCategory = serviceCategory;
+    }
+
+    public QuestionType getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(QuestionType questionType) {
+        this.questionType = questionType;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String getPossibleChoices() {
+        return possibleChoices;
+    }
+
+    public void setPossibleChoices(String possibleChoices) {
+        this.possibleChoices = possibleChoices;
+    }
 }
