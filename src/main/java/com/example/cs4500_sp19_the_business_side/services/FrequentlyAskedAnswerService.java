21604c9cebd34b300d2e2276bf824ea1be6bea83
Bailey Kane <bailey.orion.kane@gmail.com>
2019-02-08 19:14:04 -0500

    Added service functioanlity

--- src/main/java/com/example/cs4500_sp19_the_business_side/services/FrequentlyAskedAnswerService.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/services/FrequentlyAskedAnswerService.java
@@ -1,5 +1,47 @@
 package com.example.cs4500_sp19_the_business_side.services;
 
+
+import com.example.cs4500_sp19_the_business_side.models.FrequentlyAskedAnswer;
+import com.example.cs4500_sp19_the_business_side.repositories.FrequentlyAskedAnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class FrequentlyAskedAnswerService {
-    
+    @Autowired
+    FrequentlyAskedAnswerRepository frequentlyAskedAnswerRepository;
+
+    @GetMapping("/api/faqanswers")
+    public List<FrequentlyAskedAnswer> findAllFrequentlyAskedAnswers() {
+        return frequentlyAskedAnswerRepository.findAllFrequentlyAskedAnswers();
+    }
+
+    @GetMapping("/api/faqanswers/{answerId}")
+    public FrequentlyAskedAnswer findFrequentlyAskedAnswerById(
+            @PathVariable("answerId") Integer id) {
+        return frequentlyAskedAnswerRepository.findFrequentlyAskedAnswerById(id);
+    }
+
+    @PostMapping("/api/faqanswers")
+    public FrequentlyAskedAnswer createFrequentlyAskedAnswer(
+            @RequestBody FrequentlyAskedAnswer frequentlyAskedAnswer) {
+        return frequentlyAskedAnswerRepository.save(frequentlyAskedAnswer);
+    }
+
+    @PutMapping("/api/faqanswers/{answerId}")
+    public FrequentlyAskedAnswer updateFrequentlyAskedAnswer(
+            @PathVariable("answerId") Integer id,
+            @RequestBody FrequentlyAskedAnswer frequentlyAskedAnswerUpdates) {
+        FrequentlyAskedAnswer answer = frequentlyAskedAnswerRepository.findFrequentlyAskedAnswerById(id);
+        answer.setAnswer(frequentlyAskedAnswerUpdates.getAnswer());
+        answer.setTitle(frequentlyAskedAnswerUpdates.getTitle());
+        return frequentlyAskedAnswerRepository.save(answer);
+    }
+
+    @DeleteMapping("/api/faqs/{faqId}")
+    public void deleteFrequentlyAskedAnswer(@PathVariable("faqId") Integer id) {
+        frequentlyAskedAnswerRepository.deleteById(id);
+    }
 }
