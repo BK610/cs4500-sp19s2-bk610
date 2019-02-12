55d45b5249c99055bc09340cf796d5bc3c1c7aa2
Nick Bond <bond.n@husky.neu.edu>
2019-02-11 21:06:56 -0500

    allows CrossOrigin access on UserService rest controller

--- src/main/java/com/example/cs4500_sp19_the_business_side/services/UserService.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/services/UserService.java
@@ -3,6 +3,7 @@ package com.example.cs4500_sp19_the_business_side.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.cs4500_sp19_the_business_side.models.User;
 import com.example.cs4500_sp19_the_business_side.repositories.UserRepository;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class UserService {
 	@Autowired
