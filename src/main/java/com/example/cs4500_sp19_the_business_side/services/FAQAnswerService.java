80d2ad125d7901f859cf6bb48d31a892f1d1528c
Nikhil <nikhilphatak1@gmail.com>
2019-02-15 18:54:02 -0500

    fix errors in FAQAnswerService.java

--- src/main/java/com/example/cs4500_sp19_the_business_side/services/FAQAnswerService.java
+++ src/main/java/com/example/cs4500_sp19_the_business_side/services/FAQAnswerService.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.cs4500sp19s2jga.models.FrequentlyAskedAnswer;
-import com.example.cs4500sp19s2jga.repository.FAQAnswerRepository;
+import com.example.cs4500_sp19_the_business_side.models.FrequentlyAskedAnswer;
+import com.example.cs4500_sp19_the_business_side.repositories.FAQAnswerRepository;
 
 @RestController
 @CrossOrigin(origins="*")
