bed01bdbaeb178227f9f69c1dacb2a17ee7e42b7
Bailey Kane <bailey.orion.kane@gmail.com>
2019-02-19 18:14:17 -0500

    Added enumeration options

---
+++ src/main/java/com/example/cs4500_sp19_the_business_side/models/Frequency.java
@@ -0,0 +1,15 @@
+package com.example.cs4500_sp19_the_business_side.models;
+
+public enum Frequency {
+    ONETIME,
+    HOURLY,
+    DAILY,
+    WEEKLY,
+    BIWEEKLY,
+    MONTHLY,
+    YEARLY,
+    WEEKDAY,
+    WEEKEND,
+    EMERGENCY,
+    HOLIDAY;
+}
